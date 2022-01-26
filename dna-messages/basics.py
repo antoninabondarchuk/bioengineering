e1b82d25ab542a1fb2da50fe475b106a14975726
Antonina Bondarchuk <antonina.bondarchuk@dresslerconsulting.com>
2022-01-26 17:45:20 +0200

    Add get_frequency_table function

--- dna-messages/basics.py
+++ dna-messages/basics.py
@@ -3,6 +3,7 @@ finding hidden messages in DNA.
 
 Content:
     1. get_count_pattern() function.
+    2. get_frequency_table() function.
 
 The supplementary materials can be found on
 the correspondent Coursera/Stepik course here:
@@ -47,3 +48,36 @@ def get_count_pattern(text, pattern):
         if text[i:i + pattern_len] == pattern:
             count += 1
     return count
+
+
+def get_frequency_table(text, k):
+    """Calculates the quantities of occurrences
+    of all possible patterns of length k inside the text.
+
+    Iterates from index 0 to length of text
+    minus length of the pattern and plus one
+    to include the last possible match
+    as slice ([:]) function excludes the upper boundary.
+
+    Examples:
+        >>> get_frequency_table('CAAAAACTCAAA', 3)
+        {'CAA': 2, 'AAA': 4, 'AAC': 1, 'ACT': 1, 'CTC': 1, 'TCA': 1}
+
+    Args:
+        text (str): The DNA sequence to analyse.
+        k (int): Number to find k-mers (patterns of length k).
+
+    Returns:
+        frequencies (dict).
+        Mapping with patterns as keys and their counts as values.
+
+    """
+    frequencies = dict()
+    text_len_to_iterate = len(text) - k + 1
+    for i in range(text_len_to_iterate):
+        pattern = text[i:i + k]
+        if pattern not in frequencies:
+            frequencies[pattern] = 1
+        else:
+            frequencies[pattern] += 1
+    return frequencies
