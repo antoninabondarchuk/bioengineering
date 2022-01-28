929aba482c8f7e13d8c915e5e3f6a67ffea2fff6
Antonina Bondarchuk <antonina.bondarchuk@dresslerconsulting.com>
2022-01-28 23:19:47 +0200

    Add finding all neighbors

--- dna-messages/finding_ori.py
+++ dna-messages/finding_ori.py
@@ -4,6 +4,9 @@ finding origin of replication in DNA.
 Content:
     1. find_minimum_skew() function.
     2. hamming_distance() function.
+    3. approximate_pattern_matching_starts().
+    4. immediate_neighbors() function.
+    5. get_all_neighbors() function.
 
 The supplementary materials can be found on
 the correspondent Coursera/Stepik course here:
@@ -139,7 +142,7 @@ def immediate_neighbors(pattern):
         pattern (str): A part of DNA sequence to process.
 
     Returns:
-        (list[str]). List of all patterns that are different
+        neighborhood (list[str]). List of all patterns that are different
         from the given with only 1 character.
 
     """
@@ -150,4 +153,39 @@ def immediate_neighbors(pattern):
             if another_n != nucleotide:
                 neighbor = ''.join((pattern[:i], another_n, pattern[i+1:]))
                 neighborhood.append(neighbor)
+    return neighborhood
+
+
+def get_all_neighbors(pattern, max_diff):
+    """Finds all possible patterns to pattern with the maximum
+    difference in only max_diff characters.
+
+    Examples:
+        >>> get_all_neighbors('ACG', 1)
+        ['ACA', 'ACT', 'ACC', 'AAG', 'ATG', 'ACG', 'TCG', 'CCG', 'GCG', 'AGG']
+
+    Args:
+        pattern (str): A DNA sequence to process.
+        max_diff (int): Maximum number of mismatched
+        characters between pattern and substring of genome.
+
+    Returns:
+        (list[str]). List of all strings of the same length as given
+        pattern, but differ with at most max_diff number characters.
+
+    """
+    nucleotides = ['A', 'T', 'C', 'G']
+    if max_diff == 0:
+        return pattern
+    if len(pattern) == 1:
+        return nucleotides
+    neighborhood = list()
+    suffix_pattern = pattern[1:]
+    suffix_neighbors = get_all_neighbors(suffix_pattern, max_diff)
+    for txt in suffix_neighbors:
+        if hamming_distance(suffix_pattern, txt) < max_diff:
+            for nucl in nucleotides:
+                neighborhood.append(''.join((nucl, txt)))
+        else:
+            neighborhood.append(''.join((pattern[0], txt)))
     return neighborhood
